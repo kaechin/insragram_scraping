b7d612f90cca4b09c3e3d6727ebb5edf0178722e
kaechin <93870750+kaechin@users.noreply.github.com>
2023-06-04 11:45:46 +0200

    Update instagram_scraping.py

--- instagram_scraping.py
+++ instagram_scraping.py
@@ -8,19 +8,53 @@ from pprint import pprint
 import json
 from selenium_stealth import stealth
 import csv
+import pytesseract
 
-# Define the proxy server to be used
-proxy = "http://USERNAME:PASSWORD@proxy_server:PORTNUMBER"
-proxy = "server:port"
-output = {}
+# # Define the proxy server to be used
+# # This cording part is to use "Webshare" proxcy
+# proxy = "http://USERNAME:PASSWORD@proxy_server:PORTNUMBER"
+# proxy = "server:port"
+# output = {}
 
-#Funtion to prepare the browser fot scraping
+# #Funtion to prepare the browser fot scraping
+# def prepare_browser():
+
+#     # Set up Chrome options
+#     chrome_options = webdriver.ChromeOptions()
+#     proxy = "server:port"
+#     chrome_options.add_argument(f'--proxy-server={proxy}')
+#     chrome_options.add_argument("start-maximized")
+#     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#     chrome_options.add_experimental_option('useAutomationExtension', False)
+    
+#     # Start the Chrome driver
+#     driver = webdriver.Chrome('PATH OF CHROMEDRIVER')
+#     driver.get("https://www.instagram.com/")
+
+#     # Configure stealth options for Selenium
+#     # It is for helping to increase the success rate of scraping
+#     stealth(driver,
+#             user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36',
+#             languages=["en-US", "en"],
+#             vendor="Google Inc.",
+#             platform="Win32",
+#             webgl_vendor="Intel Inc.",
+#             renderer="Intel Iris OpenGL Engine",
+#             fix_hairline=False,
+#             run_on_insecure_origins=False,
+#             )
+#     return driver
+
+# This cording part is to use "Bright Data" proxcy
 def prepare_browser():
-
     # Set up Chrome options
     chrome_options = webdriver.ChromeOptions()
-    proxy = "server:port"
+
+    # Configure the Bright Data proxy
+    proxy = "http://USERNAME:PASSWORD@HOST"
     chrome_options.add_argument(f'--proxy-server={proxy}')
+
+    # Other Chrome options
     chrome_options.add_argument("start-maximized")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option('useAutomationExtension', False)
@@ -39,14 +73,20 @@ def prepare_browser():
             webgl_vendor="Intel Inc.",
             renderer="Intel Iris OpenGL Engine",
             fix_hairline=False,
-            run_on_insecure_origins=False,
-            )
+            run_on_insecure_origins=False
+    )
     return driver
 
 # Function to parse the scraped user data
 def parse_data(username, user_data):
     captions = []
     post_urls = []
+    post_type = []
+    total_likes = 0
+    total_interactions = 0
+    total_comments = 0
+    hide_like_and_view_counts = False
+
     if len(user_data['edge_owner_to_timeline_media']['edges']) > 0:
         latest_node = user_data['edge_owner_to_timeline_media']['edges'][0]
         if len(latest_node['node']['edge_media_to_caption']['edges']) > 0:
@@ -56,6 +96,22 @@ def parse_data(username, user_data):
         print(f"Post URL: {post_url}")
         post_urls.append(post_url)
 
+        # Check the type of the post
+        if latest_node['node']['is_video']:
+            post_type.append('Video')
+        else:
+            post_type.append('Photo')
+        
+        # Calculate the total number of likes, comments and interactions
+        total_likes += latest_node['node']['edge_liked_by']['count']
+        total_comments += latest_node['node']['edge_media_to_comment']['count']
+        total_interactions = total_likes + total_comments
+
+        # Check the Like and view counts disabled
+        if 'hide_like_and_view_counts' in latest_node['node']:
+            hide_like_and_view_counts = latest_node['node']['hide_like_and_view_counts']
+        #hide_like_and_view_counts = latest_node['node'].get('hide_like_and_view_counts', False)
+
     user_output = {
         'UserName': username,
         'Name': user_data['full_name'],
@@ -63,14 +119,20 @@ def parse_data(username, user_data):
         'Followers': user_data['edge_followed_by']['count'],
         'Following': user_data['edge_follow']['count'],
         'Number of Post': user_data['edge_owner_to_timeline_media']['count'],
+        'Total Likes': total_likes,
+        'Total Comments': total_comments,
+        'Total Interactions': total_interactions,
         'Posts': captions,
         #'Post URLs': post_urls,
         # Change the data type from list to string
         'Post URLs': post_urls[0] if post_urls else '',
+        'Type': post_type[0] if post_type else '',
         'Link': f"https://instagram.com/{username}",
+        'Like and view counts disabled': hide_like_and_view_counts
     }
     print(f"user_output: {user_output}")
     return user_output
+
 
 # Function to scrape user data from
 def scrape(username):
@@ -86,6 +148,7 @@ def scrape(username):
         print("Success")
         resp_body = chrome.find_element(By.TAG_NAME, "body").text
         data_json = json.loads(resp_body)
+        print("Answer" + resp_body)
         user_data = data_json['graphql']['user']
         num_posts = user_data['edge_owner_to_timeline_media']['count']
         parsed_data = parse_data(username, user_data)
@@ -93,9 +156,11 @@ def scrape(username):
         return parsed_data
 
 def main():
+    # Define the "output" dictionary
+    output = {}
     # Open a CSV file in write mode
     with open('instagram.csv', 'w', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['UserName', 'Name', 'Category', 'Followers', 'Following', 'Number of Post', 'Posts', 'Post URLs', 'Link']
+        fieldnames = ['UserName', 'Name', 'Category', 'Followers', 'Following', 'Number of Post', 'Total Likes', 'Total Comments', 'Total Interactions', 'Posts', 'Post URLs', 'Type', 'Link', 'Like and view counts disabled']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         # Write the header row
@@ -111,7 +176,7 @@ def main():
                 # user_data['Post URLs'] = '\n'.join(user_data['Post URLs'])
                 writer.writerow(user_data)
                 output[username] = user_data
+    pprint(output)
 
 if __name__ == '__main__':
     main()
-    pprint(output)
